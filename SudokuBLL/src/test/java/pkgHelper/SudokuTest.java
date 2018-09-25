28a531a5e29f9f5287d6d78235ea81ee8c13258b
Kenny <Kenny@128.4.105.248>
2018-09-24 22:55:17 -0400

    Ken Powley's Test Cases

--- SudokuBLL/src/test/java/pkgHelper/SudokuTest.java
+++ SudokuBLL/src/test/java/pkgHelper/SudokuTest.java
@@ -3,6 +3,7 @@ package pkgHelper;
 import pkgHelper.LatinSquare;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -12,38 +13,59 @@ import org.junit.Test;
 
 import pkgGame.Sudoku;
 
-public class SudokuTest { 
-	
-	
-	private int testArr[][]= {{1,2,3,4},{2,3,4,1},{3,4,2,1},{4,1,2,3}};
-	private int expectedArr[]= {2,3,4,1};
+public class SudokuTest {
+
+	private int testArr[][] = { { 1, 2, 3, 4 }, { 2, 3, 4, 1 }, { 3, 4, 2, 1 }, { 4, 1, 2, 3 } };
+	private int expectedArr[] = { 2, 3, 4, 1 };
+
 	public void getRegionTest1() {
-		
-		Sudoku s= new Sudoku(testArr);
-		
-		assertTrue(Arrays.equals(expectedArr,s.getRegion(1)));
-		
+
+		Sudoku s = new Sudoku(testArr);
+
+		assertTrue(Arrays.equals(expectedArr, s.getRegion(1)));
+
 	}
+
 	public void getRegionTest2() {
-		
-		Sudoku s= new Sudoku(testArr);
-		
-		assertFalse(Arrays.equals(expectedArr,s.getRegion(2)));
-		
+
+		Sudoku s = new Sudoku(testArr);
+
+		assertFalse(Arrays.equals(expectedArr, s.getRegion(2)));
+
 	}
-	
+
 	@Test
 	public void isSudoku_Test1() {
-		
+
 		try {
 			int[][] mySudoku = {{1, 2, 3, 4}, {3, 4, 2, 1}, {2, 1, 4, 3}, {4, 3, 1, 2}};
 			Sudoku testSudoku = new Sudoku(mySudoku);
 			assertTrue(testSudoku.isSudoku());
 		}
-		
+
 		catch (Exception e) {
 			fail("");
 		}
-		
+	}
+
+	@Test
+	public void isValidValue_Test1() {
+		try {
+			int [][] mySudoku = {{0, 2, 3, 4}, {4, 1, 2, 3}, {3, 4, 1, 2}, {2, 3, 4, 1}};
+			Sudoku testSudoku = new Sudoku(mySudoku);
+			assertTrue(testSudoku.isValueValid(0, 0, 1));}
+		catch (Exception e) {
+			fail("Has Zero");
+		}
+	}
+	@Test
+	public void isValidValue_Test2() {
+		try {
+			int [][] mySudoku = {{0, 2, 3, 4}, {4, 1, 2, 3}, {3, 4, 1, 2}, {2, 3, 4, 1}};
+			Sudoku testSudoku = new Sudoku(mySudoku);
+			assertFalse(testSudoku.isValueValid(0, 0, 2));}
+		catch (Exception e) {
+			fail("");
+		}
 	}
 }
